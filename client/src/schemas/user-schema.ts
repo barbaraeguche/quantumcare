08e58433bdc5d75cfe48daa7b29dd0efd10f630c
barbara 💌 <barbaraeguche@icloud.com>
2025-02-12 16:33:59 -0500

    minor changes 3

--- client/src/schemas/user-schema.ts
+++ client/src/schemas/user-schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 import {
-	emailRegex, phoneNumberRegex, canadianProvinces, postalCode
+	emailRegex, phoneNumberRegex, canadianProvinces, postalCodeRegex
 } from '../utils/constants.ts';
 
 export const BasicUserInfoSchema = z.object({
@@ -22,7 +22,7 @@ export const UserAddressSchema = z.object({
 	city: z.string().min(1, { message: 'Enter a valid city' }),
 	province: z.enum(canadianProvinces, { message: 'Select a province' }),
 	postalCode: z.custom<string>((val) => {
-		return typeof val === 'string' && postalCode.test(val)
+		return typeof val === 'string' && postalCodeRegex.test(val)
 	},  { message: 'Enter a valid canadian postal code' }),
 	country: z.custom<string>((val) => {
 		return typeof val === 'string' && val === 'Canada'
