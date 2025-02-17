08627a5dd1136baa355d00a12ab7ba9efa30b518
barbara 💌 <barbaraeguche@icloud.com>
2025-02-17 12:51:05 -0500

    refactoring ui

--- client/src/ui/table.tsx
+++ client/src/ui/table.tsx
@@ -21,54 +21,55 @@ export default function DataTable<TData, TValue>({ columns, data }: DataTablePro
 	
 	return (
 		<div>
-			<div className={'rounded-md border'}>
-				<Table>
-					<TableHeader>
-						{table.getHeaderGroups().map((headerGroup) => (
-							<TableRow key={headerGroup.id}>
-								{headerGroup.headers.map((header) => (
-									<TableHead key={header.id}>
-										{header.isPlaceholder
-											? null
-											: flexRender(
-												header.column.columnDef.header,
-		                    header.getContext()
-											)}
-									</TableHead>
+			<Table>
+				<TableHeader>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow
+							key={headerGroup.id}
+							className={'bg-gray-100'}
+						>
+							{headerGroup.headers.map((header) => (
+								<TableHead key={header.id}>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+											header.column.columnDef.header,
+		                  header.getContext()
+										)}
+								</TableHead>
+							))}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody>
+					{table.getRowModel().rows.length ? (
+						table.getRowModel().rows.map((row) => (
+							<TableRow
+								key={row.id}
+								data-state={row.getIsSelected() && 'selected'}
+							>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>
+										{flexRender(
+		                  cell.column.columnDef.cell,
+		                  cell.getContext(),
+		                )}
+									</TableCell>
 								))}
 							</TableRow>
-						))}
-					</TableHeader>
-					<TableBody>
-						{table.getRowModel().rows.length ? (
-							table.getRowModel().rows.map((row) => (
-								<TableRow
-									key={row.id}
-									data-state={row.getIsSelected() && 'selected'}
-								>
-									{row.getVisibleCells().map((cell) => (
-										<TableCell key={cell.id}>
-											{flexRender(
-		                    cell.column.columnDef.cell,
-		                    cell.getContext(),
-		                  )}
-										</TableCell>
-									))}
-								</TableRow>
-							))
-						) : (
-							<TableRow>
-		            <TableCell
-			            colSpan={columns.length}
-			            className={'h-24 text-center'}
-		            >
-		              No results found.
-		            </TableCell>
-		          </TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</div>
+						))
+					) : (
+						<TableRow>
+		          <TableCell
+		            colSpan={columns.length}
+		            className={'h-24 text-center'}
+		          >
+		            No results found.
+		          </TableCell>
+		        </TableRow>
+					)}
+				</TableBody>
+			</Table>
 			<div className={'flex items-center justify-end space-x-2 py-2'}>
 				<Button
 					variant={'outline'}
