3512a53d887e309a66f46344a0beeab36bf451bd
barbara 💌 <barbaraeguche@icloud.com>
2025-04-07 23:42:35 -0400

    chore: remove controlled sidebar from account page; on mobile, there would be one side menu nav that would be controlled by the icon on the header

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -1,6 +1,4 @@
-import { ElementType, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Menu, X } from 'lucide-react';
 import { useAppSelector } from '@/hooks/useAppDispatch';
 import { SidebarRoutes } from '@/lib/types';
 import {
@@ -8,7 +6,6 @@ import {
 } from '@/routes';
 
 export default function Sidebar() {
-	const [isOpen, setIsOpen] = useState(false);
 	const { role } = useAppSelector((state) => state.userSlice.user);
 	
 	const userNav = [...userRoutes];
@@ -19,71 +16,13 @@ export default function Sidebar() {
 					: []
 	)];
 	
-	// handle responsive behavior
-	useEffect(() => {
-		const handleResize = () => {
-			if (window.innerWidth >= 768) setIsOpen(true); // md breakpoint
-			else setIsOpen(false);
-		};
-		
-		// set initial state based on screen size
-		handleResize();
-		
-		// add event listener, and clean up
-		window.addEventListener('resize', handleResize);
-		return () => window.removeEventListener('resize', handleResize);
-	}, []);
-	
-	const toggleSidebar = () => setIsOpen(!isOpen);
-	
 	return (
-		<div className={'relative flex h-full z-20'}>
-			{/* sidebar - always visible on md screens and up */}
-			<div className={`transition-all duration-300 ease-in-out h-full ${
-				isOpen ? 'w-64' : 'w-0 md:w-64'
-			} ${
-				window.innerWidth < 768 ? 'absolute left-0 top-0' : 'relative'
-			}`}>
-				<div className={'h-full bg-white border-r border-gray-200 overflow-hidden'}>
-					{/* menu close button - hidden on md screens and up */}
-					<div className={'flex justify-end'}>
-						<ToggleBtn
-							Icon={X}
-							isClose={true}
-							toggleSidebar={toggleSidebar}
-						/>
-					</div>
-					
-					<nav>
-						<NavItems title={'User'} navArr={userNav}/>
-						<NavItems title={role} navArr={roleNav}/>
-					</nav>
-				</div>
-			</div>
-			
-			{/* menu open button - hidden on menu open, md screens and up */}
-			{!isOpen && (
-				<ToggleBtn
-					Icon={Menu}
-					toggleSidebar={toggleSidebar}
-				/>
-			)}
+		<div className={'w-64 h-full border-r border-gray-200 overflow-hidden'}>
+			<nav>
+				<NavItems title={'User'} navArr={userNav}/>
+				<NavItems title={role} navArr={roleNav}/>
+			</nav>
 		</div>
-	);
-}
-
-function ToggleBtn({ toggleSidebar, isClose, Icon }: {
-	toggleSidebar: () => void,
-	isClose?: boolean,
-	Icon: ElementType
-}) {
-	return (
-		<button
-			onClick={toggleSidebar}
-			className={`${isClose && 'md:hidden'} h-fit m-4 p-1 hover:cursor-pointer rounded-md hover:bg-gray-100`}
-		>
-			<Icon size={16}/>
-		</button>
 	);
 }
 
